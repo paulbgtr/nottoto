4c2b7ef90af104fb50816185cc3df62e507000a1
paulbg <paulbg@hey.com>
2023-12-18 17:30:44 +0200

    feat: add new cli module which will be parsing args and connect requests with them

---
+++ frontend/src/cli.rs
@@ -0,0 +1,26 @@
+use crate::args::Args;
+use crate::requests;
+
+pub async fn handle_args(
+    args: Args,
+    client: reqwest::Client,
+) -> Result<(), Box<dyn std::error::Error>> {
+    if let Some(file_name) = args.create {
+        let file_body = String::new();
+        requests::create(&client, file_name, file_body).await?;
+    }
+
+    if let Some(file_id) = args.update {
+        // todo: implement this
+    }
+
+    if let Some(file_id) = args.delete {
+        requests::delete(&client, file_id).await?;
+    }
+
+    if let Some(file_name) = args.find {
+        requests::find(file_name).await?;
+    }
+
+    Ok(())
+}
