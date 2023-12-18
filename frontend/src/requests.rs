d1aa61b79ff34dc04060f9029dd652717cb01485
paulbg <paulbg@hey.com>
2023-12-18 16:49:49 +0200

    feat: add delete function

--- frontend/src/requests.rs
+++ frontend/src/requests.rs
@@ -20,9 +20,11 @@ async fn create(
 
 async fn update(
     client: reqwest::Client,
+    file_id: i32,
     file_name: Option<String>,
     file_body: Option<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("http://localhost:3001/notes/{}", file_id);
     let mut data = HashMap::new();
 
     if let Some(file_name) = file_name {
@@ -33,11 +35,15 @@ async fn update(
         data.insert("file_body", file_body);
     }
 
-    let res = client
-        .post("http://localhost:3001/notes")
-        .json(&data)
-        .send()
-        .await;
+    let res = client.patch(url).json(&data).send().await;
+
+    Ok(())
+}
+
+async fn delete(client: reqwest::Client, file_id: i32) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("http://localhost:3001/notes/{}", file_id);
+
+    let res = client.delete(url).send().await?;
 
     Ok(())
 }
