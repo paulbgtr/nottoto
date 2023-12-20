7178ec9947075fa043cb741caf3d261d98c0c8a9
paulbg <paulbg@hey.com>
2023-12-20 07:42:53 +0200

    feat: use the default controller in router

--- backend/lib/nottoto_web/router.ex
+++ backend/lib/nottoto_web/router.ex
@@ -2,10 +2,11 @@ defmodule NottotoWeb.Router do
   use NottotoWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", NottotoWeb do
-    pipe_through :api
+    pipe_through(:api)
+    get("/", DefaultController, :index)
   end
 end
