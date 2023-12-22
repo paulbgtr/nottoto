97b3ae7b9ebe89ba8e849f5400c48412c9fa52a2
paulbg <paulbg@hey.com>
2023-12-22 09:23:43 +0200

    feat: impl noters controller with GET and POST funcs for notes

---
+++ backend/controllers/notes.controller.js
@@ -0,0 +1,37 @@
+import { getNotes, getNote, createNote } from "../db/queries/notes.js";
+
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await getNotes();
+    res.status(200).json(notes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+export const getNoteById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const note = await getNote(id);
+
+    res.status(200).json(note);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+export const postNote = async (req, res) => {
+  try {
+    const { title, body } = req.body;
+
+    const note = await createNote({
+      title,
+      body,
+    });
+    res.status(201).json(note);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
