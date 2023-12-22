09a67ac7d250bfe0a35ead4da5bde018bb9015a4
paulbg <paulbg@hey.com>
2023-12-22 08:26:22 +0200

    feat: add schemas file with schema for notes table

---
+++ backend/db/schemas.js
@@ -0,0 +1,9 @@
+import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+
+export const notes = pgTable("notes", {
+  id: serial("id").primaryKey(),
+  title: text("title"),
+  body: text("body"),
+  createdAt: timestamp("created_at", { withTimezone: true }).default(),
+});
+
