03f7573d934da053ddf92bfd54ab867df13f8754
paulbg <paulbg@hey.com>
2023-12-22 09:33:21 +0200

    feat: add PATCH /notes/:id

--- backend/router.js
+++ backend/router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllNotes, getNoteById, postNote } from "./controllers/notes.controller.js";
+import { getAllNotes, getNoteById, postNote, updateNoteById } from "./controllers/notes.controller.js";
 
 const router = express.Router();
 
 router.get("/notes", getAllNotes);
 router.get("/notes/:id", getNoteById);
 router.post("/notes", postNote);
+router.patch("/notes/:id", updateNoteById);
 
 export default router;
