36a742463279a5966c165bd82407b6bb2e5e1533
paulbg <paulbg@hey.com>
2023-12-23 09:07:05 +0200

    feat: impl DELETE /notes/:id

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllNotes, getNoteById, postNote, updateNoteById } from "./controllers/notes.controller.js";
+import { getAllNotes, getNoteById, postNote, updateNoteById, deleteNoteById } from "./controllers/notes.controller.js";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get("/notes", getAllNotes);
 router.get("/notes/:id", getNoteById);
 router.post("/notes", postNote);
 router.patch("/notes/:id", updateNoteById);
+router.delete("/notes/:id", deleteNoteById);
 
 export default router;
