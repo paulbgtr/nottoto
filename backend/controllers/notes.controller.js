7d43d4e28c5ec68584ad9de1f5e9b91a0db10974
paulbg <paulbg@hey.com>
2023-12-23 07:58:56 +0200

    feat: impl error handling for notes controller
    Error handling is implemented for:
    
    - getAllNotes - returns `404` if no notes are returned.
    - getNoteById - returns `404` if no note with a passed id is found.
    - postNote - returns `400` if no title is set.
    - updateNoteById - returns 400 if title and body are missing.

--- backend/controllers/notes.controller.js
+++ backend/controllers/notes.controller.js
@@ -3,6 +3,11 @@ import { getNotes, getNote, createNote, updateNote } from "../db/queries/notes.j
 export const getAllNotes = async (req, res) => {
   try {
     const notes = await getNotes();
+
+    if (!notes) {
+      res.status(404).json({ error: "No notes found" });
+    }
+
     res.status(200).json(notes);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -13,7 +18,11 @@ export const getNoteById = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const note = await getNote(id);
+    const [note] = await getNote(id);
+
+    if (!note) {
+      res.status(404).json({ error: `Note with id ${id} not found` });
+    }
 
     res.status(200).json(note);
   } catch (err) {
@@ -24,6 +33,10 @@ export const getNoteById = async (req, res) => {
 export const postNote = async (req, res) => {
   try {
     const { title, body } = req.body;
+
+    if (!title) {
+      res.status(400).json({ error: "Missing the required title value" });
+    }
 
     const note = await createNote({
       title,
@@ -39,6 +52,10 @@ export const updateNoteById = async (req, res) => {
   try {
     const { id } = req.params;
     const { title, body } = req.body;
+
+    if (!title && !body) {
+      res.status(400).json({ error: "Missing title and body values" });
+    }
 
     const note = await updateNote(id, {
       title,
