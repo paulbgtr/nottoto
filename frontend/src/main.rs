9c2b05374b08fb4cdcfdc973795d55d16827494e
paulbg <paulbg@hey.com>
2023-12-23 12:38:54 +0200

    feat: add cli mod to main and implement using it

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,9 +2,12 @@ mod args;
 mod cli;
 mod requests;
 
-use args::Args;
 use clap::Parser;
 
-fn main() {
-    let args = Args::parse();
+#[tokio::main]
+async fn main() {
+    let args = args::Args::parse();
+    let client = reqwest::Client::new();
+
+    cli::handle_args(args, client).await.unwrap();
 }
