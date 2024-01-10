a4afca1297427f8adc9b90063d4fe74b657f8d0b
paulbg <paulbg@hey.com>
2024-01-10 10:17:21 +0200

    feat(router): organize imports and setup auth routes

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,15 @@
 import express from "express";
-import { getAllNotes, getNoteById, postNote, updateNoteById, deleteNoteById } from "./controllers/notes.controller.js";
+import {
+  getAllNotes,
+  getNoteById,
+  postNote,
+  updateNoteById,
+  deleteNoteById
+} from "./controllers/notes.controller.js";
+import {
+  signup,
+  signin
+} from "./controllers/auth.controller.js"
 
 const router = express.Router();
 
@@ -8,5 +18,8 @@ router.get("/notes/:id", getNoteById);
 router.post("/notes", postNote);
 router.patch("/notes/:id", updateNoteById);
 router.delete("/notes/:id", deleteNoteById);
+
+router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
