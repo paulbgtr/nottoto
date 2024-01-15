333ae0e5ea66601ce81f6279e8f5213c070469d6
paulbg <paulbg@hey.com>
2024-01-15 10:46:56 +0200

    feat: implement is_logged_in function

--- frontend/src/auth.rs
+++ frontend/src/auth.rs
@@ -1,4 +1,14 @@
-pub fn is_logged_in() -> Result<bool, Box<dyn std::error::Error>> {
-    //todo
-    Ok(false)
+use crate::requests;
+
+pub async fn is_logged_in(client: &reqwest::Client) -> Result<bool, Box<dyn std::error::Error>> {
+    let user = requests::user_verify(&client).await;
+
+    match user {
+        Ok(_) => {
+            return Ok(true);
+        }
+        Err(_) => {
+            return Ok(false);
+        }
+    }
 }
