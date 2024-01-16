1b118dbb83bdc5642f8f2bd80a5f5e5f4ca369e0
paulbg <paulbg@hey.com>
2024-01-16 08:50:16 +0200

    feat: decide to pass the token in the req body
    This is done in order to simplify the overall logic of logging in.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -55,10 +55,9 @@ export const signin = async (req, res) => {
       userId: user.id,
     }, "secret", { expiresIn: "7d" });
 
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    })
+    res.status(200).json({
+      token
+    });
   } catch (err) {
     res.status(500).json({
       message: "Error signing in"
