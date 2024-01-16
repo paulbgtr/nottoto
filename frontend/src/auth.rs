d1c89b67ed7e69a87b60153eb29b4e213a98f438
paulbg <paulbg@hey.com>
2024-01-16 09:00:04 +0200

    feat: impl reading token and passing it to user_verify

--- frontend/src/auth.rs
+++ frontend/src/auth.rs
@@ -1,14 +1,19 @@
 use crate::requests;
+use std::fs;
 
 pub async fn is_logged_in(client: &reqwest::Client) -> Result<bool, Box<dyn std::error::Error>> {
-    let user = requests::user_verify(&client).await;
+    let token_path = "token.txt";
+    let token = fs::read_to_string(token_path);
 
-    match user {
-        Ok(_) => {
+    if let Ok(token) = token {
+        let user = requests::user_verify(&client, token).await;
+
+        if let Ok(_) = user {
             return Ok(true);
-        }
-        Err(_) => {
+        } else {
             return Ok(false);
         }
+    } else {
+        return Ok(false);
     }
 }
