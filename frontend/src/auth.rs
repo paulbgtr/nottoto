c4f6ab7854dac928f7744282f99310416eca3624
paulbg <paulbg@hey.com>
2024-01-16 09:11:49 +0200

    feat: impl user logout using filesystem

--- frontend/src/auth.rs
+++ frontend/src/auth.rs
@@ -1,6 +1,15 @@
 use crate::requests;
 use std::fs;
 
+pub fn user_logout() -> Result<(), Box<dyn std::error::Error>> {
+    let token_path = "token.txt";
+    fs::remove_file(token_path)?;
+
+    println!("You have been logged out");
+
+    Ok(())
+}
+
 pub async fn is_logged_in(client: &reqwest::Client) -> Result<bool, Box<dyn std::error::Error>> {
     let token_path = "token.txt";
     let token = fs::read_to_string(token_path);
