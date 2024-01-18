76af235842ac53e1cc80c8c16d94d95f3fc8a38e
paulbg <paulbg@hey.com>
2024-01-18 12:51:47 +0200

    feat: update notes schema to add userId

--- backend/db/schemas.js
+++ backend/db/schemas.js
@@ -1,16 +1,17 @@
-import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
-
-export const notes = pgTable("notes", {
-  id: serial("id").primaryKey(),
-  title: text("title"),
-  body: text("body"),
-  createdAt: timestamp("created_at", { withTimezone: true }).default(),
-});
+import { serial, integer, text, timestamp, pgTable } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   email: text("email").notNull(),
   password: text("password").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).default(),
+});
+
+export const notes = pgTable("notes", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").notNull().references(() => users.id),
+  title: text("title"),
+  body: text("body"),
   createdAt: timestamp("created_at", { withTimezone: true }).default(),
 });
 
